840e9e13627ac5db3179ab3d46d2c3bae880bbdf
apequito <150926883+apequito@users.noreply.github.com>
2026-05-08 16:27:58 +0100

    Version 08.05.26.14.00

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta, date as date_func
+from datetime import datetime, timedelta, date
 from io import BytesIO
 import calendar
 from flask import Flask, render_template, redirect, url_for, request, flash, send_file, jsonify
@@ -1956,7 +1956,7 @@ def imprimir_escala_mes():
                            categorias_ordem=categorias_ordem,
                            feriados=feriados,
                            ferias_lista=ferias_lista,
-                           date_func=date_func)
+                           date=date)  # ← voltar a usar 'date' simples
 
 
 
